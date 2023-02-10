b93edfd65dc8bac347b88fbae6f93f0c69ae3683
RyanHalliwellAI <ryan.halliwellai@gmail.com>
2023-02-10 12:40:53 +1030

    Implementing getWeather method

--- WeatherApp.py
+++ WeatherApp.py
@@ -7,11 +7,27 @@ load_dotenv()
 apiKey = os.getenv("API_KEY")
 
 
-
+#Getting the latutide and longitude based on location
 def getLatLon(cityName, stateCode, countryCode, APIKey):
-    response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={cityName}
-                            ,{stateCode},{countryCode}&appid={APIKey}')
-    
-    print(response)
+    #Calling API, and saving it into a valid JSON object
+    ApiResponse = requests.get(
+        f'http://api.openweathermap.org/geo/1.0/direct?q={cityName},{stateCode},{countryCode}&appid={APIKey}'
+    ).json()
 
-getLatLon("Toronto", "ON", "Canada", apiKey)
+    locationData = ApiResponse[0]
+    Lat = locationData.get('lat')
+    Lon = locationData.get('lon')
+    
+    return Lat, Lon
+
+#returns the current weather conditions for the lon and lat
+def getWeatherConditions(lat, lon, ApiKey):
+    ApiResponse = requests.get(
+        f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={ApiKey}'
+    ).json()
+
+
+
+if __name__ == "__main__":
+    lat, lon = getLatLon("Toronto", "ON", "Canada", apiKey)
+    getWeatherConditions(lat, lon, apiKey)
